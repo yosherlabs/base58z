60a8c1cd9250a6e981299305b26937f3163c0176
Jon-Eric Cook <joneric.cook@gmail.com>
2023-03-29 14:57:07 -0700

    add missing z

--- build.zig
+++ build.zig
@@ -5,17 +5,17 @@ pub fn build(b: *std.build.Builder) void {
 
    // builds the library as a static library
     {
-    const lib = b.addStaticLibrary("base58", "src/base58.zig");
+    const lib = b.addStaticLibrary("base58z", "src/base58z.zig");
     lib.setBuildMode(mode);
     lib.install();
     }
 
     // builds and runs the tests
     {
-    const base58_tests = b.addTest("src/base58.zig");
-    base58_tests.setBuildMode(mode);
-    base58_tests.use_stage1 = true;
+    const base58z_tests = b.addTest("src/base58z.zig");
+    base58z_tests.setBuildMode(mode);
+    base58z_tests.use_stage1 = true;
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&base58_tests.step);
+    test_step.dependOn(&base58z_tests.step);
     }
 }
